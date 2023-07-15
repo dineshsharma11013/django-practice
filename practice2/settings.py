97c234bd1a1950ba8e7f5c52355a9b0f5d075f9b
Dinesh Sharma <dineshsharma11013@gmail.com>
2023-07-15 17:53:34 +0530

    updated

--- practice2/settings.py
+++ practice2/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = "/mediaa/"
-MEDAI_ROOT = BASE_DIR
+MEDIA_ROOT = BASE_DIR
 
 
 # Default primary key field type
